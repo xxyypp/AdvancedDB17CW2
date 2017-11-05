03f4360096576465c08c0722467ddc555e8d5750
xxyypp <yangpuypyp@outlook.com>
2017-11-05 15:44:59 +0000

    Create

---
+++ run.sh
@@ -0,0 +1,13 @@
+echo "-------------------------------------"
+echo "Start to build the project!"
+echo "-------------------------------------"
+cmake --build ~/AdvancedDB17CW1/build
+echo "Finished build..."
+
+echo " "
+echo "-------------------------------------"
+echo "Run the project..."
+echo "-------------------------------------"
+~/AdvancedDB17CW1/build/ADBCoursework1
+echo " "
+echo "Finshed..."
